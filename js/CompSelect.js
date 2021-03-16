59b76d5b0991ac3fb2ec8d7d19e9e73fa4c3fe16
HSIN <jack1234552000@yahoo.com.tw>
2021-03-16 10:03:44 +0800

    202103161002
    新增搜尋的常用關鍵字列表  (學習dataList的標籤的使用)
    
    將名稱搜尋也改為
    鍵入關鍵字後會一併清空其他欄位  並把所有屬性 物種偏向改為All
    的設定

--- js/CompSelect.js
+++ js/CompSelect.js
@@ -26,6 +26,9 @@ var testComp  = Vue.component("xxxx", {
 				"能學法術","肉體強化","恢復","連擊","豬頭","裝死","賜福","威脅",
 				"轉換","吸血","推車","犧牲","亡命一擊","三連擊","吸魂術","拉拉舞"
 			],
+			nameOption:["太郎","公主","士兵","小狗","鯉魚"],
+			dropOption:["幻獸營養劑","玻璃種子","生命核心","宇宙奧秘","貴賓卡"],
+			mapOption:["魔幣","稀有","威奇","龍宮","鬼"],
 			skillSelect:[],
 			elementSelect:"闇",
 			speciesDirSelect:"智",
@@ -67,9 +70,18 @@ var testComp  = Vue.component("xxxx", {
 						</div>
 						<div class="col-6"  >
 							<div class="row" style="height:25vh;">
-								<div>名稱<input type='search' v-model="nameSelect" placeholder="輸入名稱關鍵字"></div>
-								<div>掉落<input type='search' v-model="dropSelect" placeholder="輸入掉落物關鍵字"></div>
-								<div>出處<input type='search' v-model="mapSelect" placeholder="輸入取得出處關鍵字"></div>
+								<div>名稱<input type='search' v-model="nameSelect" placeholder="輸入名稱關鍵字" list="nameInput"></div>
+								<div>掉落<input type='search' v-model="dropSelect" placeholder="輸入掉落物關鍵字" list="dropInput"></div>
+								<div>出處<input type='search' v-model="mapSelect" placeholder="輸入取得出處關鍵字" list="mapInput"></div>
+								<datalist id="nameInput">
+									<option v-for="(v,k) in nameOption" :value="v">
+								</datalist>
+								<datalist id="dropInput">
+									<option v-for="(v,k) in dropOption" :value="v">
+								</datalist>
+								<datalist id="mapInput">
+									<option v-for="(v,k) in mapOption" :value="v">
+								</datalist>
 							</div>
 							<div class="row" style="height:40vh;" @contextmenu.prevent @mousedown="selectAllSkill($event)">
 								<span  class="mr-3" v-for="(v,k) in skillOption">
@@ -109,6 +121,15 @@ var testComp  = Vue.component("xxxx", {
 			select:function(){
 					this.$emit("update-pet",this.select);
 				
+			},
+			nameSelect:function(){
+				if(this.nameSelect == ""){return;}
+				
+				this.speciesDirSelect = "All";
+				this.elementSelect = "All";
+				this.dropSelect = "";
+				this.mapSelect = "";
+				this.skillSelect = [];
 			},
 			dropSelect:function(){
 				if(this.dropSelect == ""){return;}

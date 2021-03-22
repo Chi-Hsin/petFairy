19181d3c5f055357c0bb503866995ca03a079211
HSIN <jack1234552000@yahoo.com.tw>
2021-03-22 15:27:52 +0800

    202103221527
    增加開頭加載動畫(故事-第五隻老鼠)

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ var indexData = new Vue({
 				boxContent:[],
 				petDropData:{},//從BOX拉過來的物品資料
 				fusionResult:{},
-				// tugienLight:[],
+				vCloakImageDisplay:0,
 				navBarStyle:{background:"#343a40",color:"rgba(255,255,255,.5)"},//導航列背景樣式
 				petFilter:[
 					{"id":212,"name":"黑鼠","level":"10~12","species":"老鼠","speciesDir":"智","element":"闇","skillAmount":3,"str":"14","vit":"14","agi":"16","int":"16","luk":"16","chm":"14","life":"97","drop":"黑鼠卡 黑鼠娃娃 重藤棍 青銅鎧甲 煤 青銅礦 黑色鈕釦","skill":"連擊、裝死、轉換、吸血、亡命一擊、詛咒術、毒擊術","map":"青鳥城外","圖片":"V"}
